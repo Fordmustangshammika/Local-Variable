5957ffa3db775b79f72d839aed05c457ab12d2fb
Shammika Dilshan <31384200+Fordmustangshammika@users.noreply.github.com>
2023-10-22 12:11:46 +0400

    Add files via upload

---
+++ Project/LocalVariable/Iphone.java
@@ -0,0 +1,19 @@
+public class Iphone {
+
+    String name;
+    String color;
+    int battery;
+    double price;
+
+    public void call(){
+
+        //local variable
+        String callName = "Shammika";
+        //Concatenate
+        System.out.println("Calling to " + callName);
+    }
+    public void sendText(){}
+    public void findPlaces(){}
+    public void takePhotos(){}
+
+}
---
+++ Project/LocalVariable/MobileShop.java
@@ -0,0 +1,12 @@
+public class MobileShop {
+
+    public static void main(String[] args) {
+
+        //Create an Object
+        Iphone iphone_1 = new Iphone();
+        iphone_1.call();
+
+
+
+    }
+}
